8dbcd4b218822a93187d2906db77a3a2ad952d4f
Ivan Neeson <zzymyn@gmail.com>
2026-06-17 13:44:07 +1000

    Start on CPU.

--- Sharptari/Sharptari.Lib/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Sharptari.Lib
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Sharptari/Sharptari.Lib/Mos6502Registers.cs
@@ -0,0 +1,78 @@
+﻿namespace Sharptari.Lib;
+
+public struct Mos6502Registers
+{
+    public const byte FlagNegativeMask = 0b1000_0000;
+    public const byte FlagOverflowMask = 0b0100_0000;
+    public const byte FlagUnusedMask = 0b0010_0000;
+    public const byte FlagBreakMask = 0b0001_0000;
+    public const byte FlagDecimalMask = 0b0000_1000;
+    public const byte FlagInterruptDisableMask = 0b0000_0100;
+    public const byte FlagZeroMask = 0b0000_0010;
+    public const byte FlagCarryMask = 0b0000_0001;
+
+    public byte A;
+    public byte P;
+    public ushort PC;
+    public byte S;
+    public byte X;
+    public byte Y;
+
+    public bool FlagNegative
+    {
+        readonly get => ReadFlag(FlagNegativeMask);
+        set => WriteFlag(FlagNegativeMask, value);
+    }
+
+    public bool FlagOverflow
+    {
+        readonly get => ReadFlag(FlagOverflowMask);
+        set => WriteFlag(FlagOverflowMask, value);
+    }
+
+    public bool FlagUnused
+    {
+        readonly get => ReadFlag(FlagUnusedMask);
+        set => WriteFlag(FlagUnusedMask, value);
+    }
+
+    public bool FlagBreak
+    {
+        readonly get => ReadFlag(FlagBreakMask);
+        set => WriteFlag(FlagBreakMask, value);
+    }
+
+    public bool FlagDecimal
+    {
+        readonly get => ReadFlag(FlagDecimalMask);
+        set => WriteFlag(FlagDecimalMask, value);
+    }
+
+    public bool FlagInterruptDisable
+    {
+        readonly get => ReadFlag(FlagInterruptDisableMask);
+        set => WriteFlag(FlagInterruptDisableMask, value);
+    }
+
+    public bool FlagZero
+    {
+        readonly get => ReadFlag(FlagZeroMask);
+        set => WriteFlag(FlagZeroMask, value);
+    }
+
+    public bool FlagCarry
+    {
+        readonly get => ReadFlag(FlagCarryMask);
+        set => WriteFlag(FlagCarryMask, value);
+    }
+
+    private readonly bool ReadFlag(byte mask) => (P & mask) != 0;
+
+    private void WriteFlag(byte mask, bool value)
+    {
+        if (value)
+            P |= mask;
+        else
+            P &= (byte)~mask;
+    }
+}
