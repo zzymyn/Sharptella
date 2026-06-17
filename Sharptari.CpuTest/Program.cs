3c896f18c0a1446677011c1fc5f79a7eaa65af3a
Ivan Neeson <zzymyn@gmail.com>
2026-06-17 15:28:17 +1000

    Move things around.

---
+++ Sharptari.CpuTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var testDir = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "..", "Ext", "SingleStepTests", "65x02", "6502"));
+
+        var testFiles = Directory.EnumerateFiles(testDir, "*.json").ToList();
+
+        foreach (var testFile in testFiles)
+        {
+            var testData = System.Text.Json.JsonSerializer.Deserialize<Sharptari.CpuTest.TestData>(File.ReadAllText(testFile));
+            if (testData is null)
+            {
+                Console.WriteLine($"Failed to read test data from {testFile}");
+                continue;
+            }
+            Console.WriteLine($"Read test data from {testFile}: {testData.Name}");
+        }
+    }
+}
---
+++ Sharptari.CpuTest/TestData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Sharptari.CpuTest;
+
+internal sealed class TestData
+{
+    [JsonPropertyName("name")]
+    public string? Name;
+
+    [JsonPropertyName("initial")]
+    public CpuState? Initial;
+
+    [JsonPropertyName("final")]
+    public CpuState? Final;
+
+    // Cycles contain heterogeneous data: [address (int), value (int), type (string)]
+    // We use JsonElement[] to read this safely without crashing
+    [JsonPropertyName("cycles")]
+    public JsonElement[]?[]? Cycles;
+}
+
+public sealed class CpuState
+{
+    [JsonPropertyName("pc")]
+    public ushort PC;
+
+    [JsonPropertyName("s")]
+    public byte S;
+
+    [JsonPropertyName("a")]
+    public byte A;
+
+    [JsonPropertyName("x")]
+    public byte X;
+
+    [JsonPropertyName("y")]
+    public byte Y;
+
+    [JsonPropertyName("p")]
+    public byte P;
+
+    [JsonPropertyName("ram")]
+    public int[]?[]? Ram;
+}
