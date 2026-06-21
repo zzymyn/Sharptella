140f62d4b226cec3c1961376f939b2a4f8aa2945
Ivan Neeson <zzymyn@gmail.com>
2026-06-21 23:57:45 +1000

    Start the long road to TIA.

--- Sharptari.Lib/Atari2600Tia.cs
+++ Sharptari.Lib/Atari2600Tia.cs
@@ -11,12 +11,64 @@ public sealed class Atari2600Tia
     private const ushort RegisterReadMask = 0b0000_0000_0000_1111;
     private const ushort RegisterWriteMask = 0b0000_0000_0011_1111;
 
+    private const int ScanlineLength = 228;
+    private const int HBlankLength = 68;
+
+    private bool m_HasFrameReady = false;
+    private List<byte> m_GeneratedPixels = [];
+    private List<byte> m_PrevGeneratedPixels = [];
+    private readonly byte[] m_CurrentScalinePixels = new byte[4 * ScanlineLength];
+    private int m_CurrentScanline = 0;
+    private int m_CurrentScanlineCycle = 0;
+
+    private bool m_VBlank = false;
+    private bool m_VSync = false;
+
+    public bool HasFrameReady => m_HasFrameReady;
+    public IReadOnlyList<byte> FramePixels => m_PrevGeneratedPixels;
+
+    public void ClearFrameReady()
+    {
+        m_HasFrameReady = false;
+    }
+
     public void Reboot()
     {
+        m_GeneratedPixels.Clear();
+        m_CurrentScanline = 0;
+        m_CurrentScanlineCycle = 0;
+        m_VBlank = true;
+        m_VSync = false;
     }
 
     public void Step()
     {
+        int i = 4 * m_CurrentScanlineCycle;
+
+        // TODO: write a colour:
+        if (m_VBlank || m_CurrentScanlineCycle < HBlankLength)
+        {
+            m_CurrentScalinePixels[i + 0] = 0;
+            m_CurrentScalinePixels[i + 1] = 0;
+            m_CurrentScalinePixels[i + 2] = 0;
+            m_CurrentScalinePixels[i + 3] = 255;
+        }
+        else
+        {
+            // Random Magenta for now, to show that we're generating pixels:
+            m_CurrentScalinePixels[i + 0] = 255;
+            m_CurrentScalinePixels[i + 1] = 0;
+            m_CurrentScalinePixels[i + 2] = 255;
+            m_CurrentScalinePixels[i + 3] = 255;
+        }
+
+        ++m_CurrentScanlineCycle;
+        if (m_CurrentScanlineCycle >= ScanlineLength)
+        {
+            m_GeneratedPixels.AddRange(m_CurrentScalinePixels);
+            m_CurrentScanlineCycle = 0;
+            ++m_CurrentScanline;
+        }
     }
 
     public int TryRead(ushort address)
@@ -80,9 +132,13 @@ public sealed class Atari2600Tia
         {
             case 0x00:
                 // VSYNC
+                SetVsync((value & 0b0000_0010) != 0);
                 break;
             case 0x01:
                 // VBLANK
+                SetVBlank((value & 0b0000_0010) != 0);
+                SetTriggerButtonLatches((value & 0b0100_0000) != 0);
+                SetPaddleCapacitorDumpToGround((value & 0b1000_0000) != 0);
                 break;
             case 0x02:
                 // WSYNC
@@ -214,5 +270,45 @@ public sealed class Atari2600Tia
                 // CXCLR
                 break;
         }
+    }
+
+    private void SetVsync(bool value)
+    {
+        var oldVSync = m_VSync;
+        m_VSync = value;
+
+        if (oldVSync != m_VSync)
+        {
+            Console.WriteLine($"VSYNC: {m_VSync} @ scanline {m_CurrentScanline}");
+        }
+
+        if (m_VSync && !oldVSync)
+        {        
+            m_HasFrameReady = true;
+            (m_GeneratedPixels, m_PrevGeneratedPixels) = (m_PrevGeneratedPixels, m_GeneratedPixels);
+            m_GeneratedPixels.Clear();
+            m_CurrentScanline = 0;
+        }
+    }
+
+    private void SetVBlank(bool value)
+    {
+        var oldVBlank = m_VBlank;
+        m_VBlank = value;
+
+        if (oldVBlank != m_VBlank)
+        {
+            Console.WriteLine($"VBLANK: {m_VBlank} @ scanline {m_CurrentScanline}");
+        }
+    }
+
+    private void SetTriggerButtonLatches(bool value)
+    {
+        // TODO
+    }
+
+    private void SetPaddleCapacitorDumpToGround(bool value)
+    {
+        // TODO
     }
 }
