21a490e27701d97c26ce21c1393a2d396749afb5
Ivan Neeson <zzymyn@gmail.com>
2026-06-21 15:33:30 +1000

    Riot chip

--- Sharptari.Lib/Mos6532Riot.cs
+++ Sharptari.Lib/Mos6532Riot.cs
@@ -20,6 +20,16 @@ public sealed class Mos6532Riot
 
     private readonly byte[] m_Ram = new byte[128];
 
+    // TODO: read controller inputs for SWCHA:
+    private byte m_SWCHA = 0b1111_1111;
+    private byte m_ORA;
+    private byte m_SWACNT;
+
+    // TODO: read switch inputs for SWCHB:
+    private byte m_SWCHB = 0b0011_1111;
+    private byte m_ORB;
+    private byte m_SWBCNT;
+
     private byte m_Timer;
     private ushort m_Prescaler;
     private ushort m_PrescalerMask;
@@ -27,10 +37,17 @@ public sealed class Mos6532Riot
 
     public void Reboot()
     {
+        Array.Clear(m_Ram, 0, m_Ram.Length);
+
         m_Timer = 255;
         m_Prescaler = 0;
         m_PrescalerMask = 1023;
         m_FlagTimer = false;
+
+        m_ORA = 0;
+        m_SWACNT = 0;
+        m_ORB = 0;
+        m_SWBCNT = 0;
     }
 
     public void Step()
@@ -56,13 +73,20 @@ public sealed class Mos6532Riot
             switch (address)
             {
                 case 0x00:
-                    return 0x00; // SWCHA
+                    // SWCHA
+                    // m_SWCHA being zero will always pull to 0, even if SWACNT is 1:
+                    // simplify: (m_SWCHA & ~m_SWACNT) | (m_SWCHA & m_ORA & m_SWACNT)
+                    return 0xFF & m_SWCHA & (~m_SWACNT | m_ORA);
                 case 0x01:
-                    return 0x00; // SWACNT
+                    // SWACNT
+                    return m_SWACNT;
                 case 0x02:
-                    return 0x00; // SWCHB
+                    // SWCHB
+                    // Simple mux between SWCHB and ORB based on SWBCNT:
+                    return 0xFF & ((m_SWBCNT & m_ORB) | (~m_SWBCNT & m_SWCHB));
                 case 0x03:
-                    return 0x00; // SWBCNT
+                    // SWBCNT
+                    return m_SWBCNT;
                 case 0x04:
                 case 0x06:
                     // INTIM
@@ -100,13 +124,21 @@ public sealed class Mos6532Riot
             switch (address)
             {
                 case 0x00:
-                    break; // SWCHA
+                    // SWCHA
+                    m_ORA = value;
+                    break;
                 case 0x01:
-                    break; // SWACNT
+                    // SWACNT
+                    m_SWACNT = value;
+                    break;
                 case 0x02:
-                    break; // SWCHB
+                    // SWCHB
+                    m_ORB = value;
+                    break;
                 case 0x03:
-                    break; // SWBCNT
+                    // SWBCNT
+                    m_SWBCNT = value;
+                    break;
                 case 0x04:
                     // 0
                     StartTimer(value, 0);
